c85a3d0cc070adeb6f4c5d0bea6ef7dead69f396
Sergei Melnichuk <serhii.melnichuk@gmail.com>
2020-08-18 21:42:33 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Are you over 18?", "Yes");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your surname?', '');
+// answers[2] = prompt('How old you?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Sergei';
+
+// alert(`Привет, ${user}`);
+
+console.log('arr' + " - object");
+console.log(4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
