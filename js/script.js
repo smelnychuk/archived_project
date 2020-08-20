242ec71b9fafdbc3ba0f883797500529afbca80d
Sergei Melnichuk <serhii.melnichuk@gmail.cim>
2020-08-20 21:55:41 +0300

    update script_2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
-"use strict";
+/* Задание на урок:
 
-const numberOfFilms = +prompt('How many movies have you seen?', '');
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+'use strict';
+
+// Код возьмите из предыдущего домашнего задания
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,10 +26,10 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('What was the last movie you saw?', ''),
-      b = prompt('How mach do you rate it?', ''),
-      c = prompt('What was the last movie you saw?', ''),
-      d = prompt('How mach do you rate it?', '');
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
