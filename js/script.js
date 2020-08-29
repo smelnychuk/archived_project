3ad42fc92d525a5931362a6c51dd2571d4a66507
Sergei Melnichuk <serhii.melnichuk@gmail.cim>
2020-08-29 13:00:57 +0300

    second practice

--- js/script.js
+++ js/script.js
@@ -16,22 +16,43 @@
 
 // Код возьмите из предыдущего домашнего задания
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+for (let i = 0; i < 1; i++) {
+    const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+    if (numberOfFilms >= 0 && numberOfFilms < 10) {
+        alert('Просмотрено довольно мало фильмов');
+    } else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+        alert('Вы классический зритель');
+    } else if (numberOfFilms > 30) {
+        alert('Вы киноман');
+    } else if (numberOfFilms == null && numberOfFilms == '') {
+        i--;
+    } else {
+        i--;
+    }
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+    const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    console.log(personalMovieDB);
+}
 
-console.log(personalMovieDB);
+
+// for (let i = 0; i < 1; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length <= 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }          
+// }
+
