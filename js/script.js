c93b8325201d17c6eb8521801cfaa8c5b42565f0
Sergei Melnichuk <serhii.melnichuk@gmail.cim>
2020-08-29 21:39:09 +0300

    third_practice

--- js/script.js
+++ js/script.js
@@ -12,31 +12,138 @@
 
 4) Потренироваться и переписать цикл еще двумя способами*/
 
+// 'use strict';
+
+// Код возьмите из предыдущего домашнего задания
+
+// for (let i = 0; i < 1; i++) {
+//     let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+//     if (numberOfFilms >= 0 && numberOfFilms < 10) {
+//         alert('Просмотрено довольно мало фильмов');
+//     } else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+//         alert('Вы классический зритель');
+//     } else if (numberOfFilms > 30) {
+//         alert('Вы киноман');
+//     } else {
+//         i--;
+//     }
+
+//     const personalMovieDB = {
+//         count: numberOfFilms,
+//         movies: {},
+//         actors: {},
+//         genres: [],
+//         privat: false
+//     };
+
+//     for (let i = 0; i < 2; i++) {
+//         const a = prompt('Один из последних просмотренных фильмов?', ''),
+//               b = prompt('На сколько оцените его?', '');
+    
+//         if (a != null && b != null && a != '' && b != '' && a.length <= 50) {
+//             personalMovieDB.movies[a] = b;
+//             console.log('done');
+//         } else {
+//             console.log('error');
+//             i--;
+//         }          
+//     }
+    
+//     console.log(personalMovieDB);
+// }
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     let num = 10;
+//     console.log(num);
+// }
+
+// showFirstMessage('Hello World!');
+// console.log(num);
+
+// function calc(a, b) {
+//     return a + b;
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+// function ret() {
+//     let num = 50;
+
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const longer = function() {
+//     console.log("Hello");
+// };
+
+// longer();
+
+// const calc = (a, b) => a + b;
+// console.log(calc(5, 6));
+
+// const str = "test";
+// console.log(str.toUpperCase());
+
+// const fruit = "Some fruit";
+// console.log(fruit.indexOf('fruit'));
+
+// const logg = "Hello world";
+// console.log(logg.slice(6));
+
+// console.log(logg.substr(6, 5));
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+// const test = "12.2px";
+// console.log(parseFloat(test));
+
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
 'use strict';
 
 // Код возьмите из предыдущего домашнего задания
 
-for (let i = 0; i < 1; i++) {
-    let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
 
-    if (numberOfFilms >= 0 && numberOfFilms < 10) {
-        alert('Просмотрено довольно мало фильмов');
-    } else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
-        alert('Вы классический зритель');
-    } else if (numberOfFilms > 30) {
-        alert('Вы киноман');
-    } else {
-        i--;
+function start() {
+    numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
     }
+}
 
-    const personalMovieDB = {
-        count: numberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
-    };
+// start();
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+function rememberMyFilms() {
     for (let i = 0; i < 2; i++) {
         const a = prompt('Один из последних просмотренных фильмов?', ''),
               b = prompt('На сколько оцените его?', '');
@@ -49,7 +156,38 @@ for (let i = 0; i < 1; i++) {
             i--;
         }          
     }
-    
-    console.log(personalMovieDB);
-
 }
+
+// rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (numberOfFilms >= 0 && numberOfFilms < 10) {
+        alert('Просмотрено довольно мало фильмов');
+    } else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+        alert('Вы классический зритель');
+    } else if (numberOfFilms > 30) {
+        alert('Вы киноман');
+    } else {
+        alert('Произошла ошибка');
+    }
+}
+
+// detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden){
+        console.log(personalMovieDB);
+    }
+}
+
+// showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt (`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
+
+console.log(personalMovieDB);
